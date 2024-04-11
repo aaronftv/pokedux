455304034ec06501d193d25799836635832d970b
Aaron Ftv <aaronfuentevilla@gmail.com>
2024-04-11 02:43:09 -0500

    Adding Redux integration with Connect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Col } from 'antd';
+import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
+import logo from './statics/logo.svg';
+import { getPokemon } from './api';
+import { setPokemons as setPokemonsAction } from './actions';
 import './App.css';
 
-function App() {
+function App({pokemons, setPokemons}) {
+  console.log("🚀 ~ file: App.js:12 ~ App ~ pokemons:", pokemons);
+  //const [pokemons, setPokemons] = useState([]);
+  useEffect(()=> {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemon();
+      setPokemons(pokemonsRes);
+    };
+    fetchPokemons();
+    //fetchPokemons(pokemons);
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Col span={4} offset={10}>
+        <img src={logo} alt='Pokedux' /> 
+      </Col>
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
+      <PokemonList pokemons={pokemons}/>
     </div>
   );
 }
 
-export default App;
+//These 2 vars below are required to connect our component to Redux, names are by convention
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToPros = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsAction(value))
+});
+
+//This below is also required for Redux integration
+export default connect(mapStateToProps, mapDispatchToPros)(App);
---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,19 @@
+import PokemonCard from "./PokemonCard";
+import './PokemonList.css';
+
+const PokemonList = ({ pokemons }) => {
+    // console.log("🚀 ~ file: PokemonList.jsx:5 ~ PokemonList ~ pokemons:", pokemons);
+    return(
+        <div className='PokemonList'>
+            {pokemons.map((pokemon) => {
+                return <PokemonCard name={pokemon.name} key={pokemon.name}/>;
+            })}
+        </div>
+    );
+};
+
+PokemonList.defaultProps ={
+    pokemons: Array(10).fill(''),
+};
+
+export default PokemonList;
